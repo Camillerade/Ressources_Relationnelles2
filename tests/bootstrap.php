ee1911e71d553fcf5bea4c80781a7da429690998
Camillerade <radecamille@outlook.fr>
2023-04-27 13:35:35 +0200

    modifs

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,10 +2,10 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require_once dirname(__DIR__).'/vendor/autoload.php';
+include dirname(__DIR__).'/vendor/autoload.php';
 
 if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
-    require_once dirname(__DIR__).'/config/bootstrap.php';
+    include dirname(__DIR__).'/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }

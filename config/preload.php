7714a1779ea3f76a771174d1116bfd4c980992a4
Camillerade <radecamille@outlook.fr>
2023-04-27 13:44:40 +0200

    modifs

--- config/preload.php
+++ config/preload.php
@@ -1,5 +1,5 @@
 <?php
 
 if (file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')) {
-    INCLUDE dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php';
+    include_once dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php';
 }
